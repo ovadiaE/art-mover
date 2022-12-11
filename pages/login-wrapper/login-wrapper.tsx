5d8b964a0868f627d61f5669a070d7650b32897a
ovadiaE <ovadia.esq@gmail.com>
2022-12-11 15:45:43 +0200

    scaffolding sign in page

--- pages/login-wrapper/login-wrapper.tsx
+++ pages/login-wrapper/login-wrapper.tsx
@@ -1,12 +1,15 @@
 import Login from "../login/login"
+import styles from './login-wrapper.module.scss'
 
 /**
  * This is a wrapper for the login/create account components
- * @returns a wrapper for which the css elements of smaller components will be relative to
+ * @returns a wrapper for which the css elements of 2 smaller components will be relative to
  */
 export default function Wrapper() {
     return (
-        <Login/>
+        <div className={styles.wrapper}>
+            <Login/>
+        </div>
     )
 }
 
--- pages/login/login.tsx
+++ pages/login/login.tsx
@@ -1,21 +1,27 @@
 import styles from "./login.module.scss"
 
-import { Input, InputLabel, FormControl, FormHelperText } from '@mui/material';
+import { Input, InputLabel, FormControl, Button } from '@mui/material';
 
 
 export default function Login () {
     return (
         <> 
         <form className={styles.login}>
+            
             <FormControl fullWidth={false}>
-                <InputLabel htmlFor="my-input">Email address</InputLabel>
+                <InputLabel htmlFor="my-input">Email or Username</InputLabel>
                 <Input id="my-input" aria-describedby="my-helper-text" />
             </FormControl>
 
             <FormControl>
-                <InputLabel htmlFor="my-input">Email address</InputLabel>
-                <Input id="my-input" aria-describedby="my-helper-text" />
+                <InputLabel htmlFor="my-input">Password</InputLabel>
+                <Input type="password" id="my-input" aria-describedby="my-helper-text" />
             </FormControl>
+           
+            <Button variant="text">Log-in</Button>        
+            <Button variant="text">Sign-up</Button>        
+
+        
         </form>
     </>
 )
