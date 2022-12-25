921d1a12907a6fef8905d7dcd1ea71347c9e836e
ovadiaE <ovadia.esq@gmail.com>
2022-12-25 17:06:36 +0200

    connected register component to backend, added cors

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -11,13 +11,16 @@ export const registerProducer = async (req: Request, res: Response) => {
             return
         }
         const success = await UserService.registerProducer(req.body.email, req.body.password)
+        
         if (!success) {
             return res.status(400).send({
                 message: 'something went wrong'
             })
         }
+        
         res.send({})
-    } catch (error) {
+    } 
+    catch (error) {
         console.log('registerProducer failed with error:', error)
         res.sendStatus(500)
     }
